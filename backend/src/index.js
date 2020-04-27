f0e6d36ff5f8fbbd6f666be31986ee94ef31dcb6
Vinicius Oliveira de Freitas <vinicius.vof@outlook.com>
2020-04-27 16:53:54 -0300

    Hello World

---
+++ backend/src/index.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const app = express();
+const port = 3333;
+
+app.get('/', (req, res) => res.send("Hello World"));
+app.listen(port, () => console.log("Rodando"));
