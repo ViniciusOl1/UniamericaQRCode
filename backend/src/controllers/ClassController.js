c251e94d6035ef27b0157bf597dd51fbf0f3565d
Vinicius Oliveira de Freitas <vinicius.vof@outlook.com>
2020-05-06 23:42:17 -0300

    Controller QRCode

---
+++ backend/src/controllers/ClassController.js
@@ -0,0 +1,23 @@
+const connection = require('../database/connection');
+const moment = require('moment-timezone');
+const cod = require('qr-image');
+const fs = require('fs');
+module.exports = {
+    async index(req, res){
+        const data  = moment.tz("America/Sao_Paulo").format("DD-MM-YYYY");
+        const qr = cod.image(data, { type: 'svg' });
+        qrcode = `./src/assets/qrcode/${data}.svg`;
+        if (fs.existsSync(qrcode)){
+            res.type('svg');
+            qr.pipe(res);
+        }else {
+            await connection('class').insert({
+                data,
+                qrcode
+            });
+            qr.pipe(fs.createWriteStream(`./src/assets/qrcode/${data}.svg`));
+            res.type('svg');
+            return qr.pipe(res);
+        }   
+    },
+}
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const AlunosController = require('./controllers/AlunosController');
 const ProfessoresController = require('./controllers/ProfessoresController');
+const ClassController = require('./controllers/ClassController');
 const SessionController = require('./controllers/SessionController');
 
 const routes = express.Router();
@@ -11,5 +12,8 @@ routes.get('/professores', ProfessoresController.index);
 routes.post('/professores', ProfessoresController.create);
 routes.delete('/professores/:id', ProfessoresController.delete);
 routes.put('/professores/:id', ProfessoresController.update);
+
+routes.post('/aula', ClassController.index);
+routes.get('/aula', ClassController.index);
 
 module.exports = routes;
