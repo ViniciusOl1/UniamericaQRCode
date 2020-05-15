05d7b3e95a16fc42bc28aaaccce93115631554f6
Vinicius Oliveira de Freitas <vinicius.vof@outlook.com>
2020-05-15 16:19:28 -0300

    Formulário de Edição Professores

--- backend/src/controllers/ProfessoresController.js
+++ backend/src/controllers/ProfessoresController.js
@@ -1,10 +1,17 @@
 const connection = require('../database/connection');
 module.exports = {
-    async index(req, res){
-        const professores = await connection('teachers').select('*');
-        return res.json(professores);
+    async index(req, res) {
+        const { id } = req.params;
+        if (id) {
+            const professor = await connection('teachers').select('*')
+                .where('id', id).first();
+            return res.json(professor);
+        } else {
+            const professores = await connection('teachers').select('*');
+            return res.json(professores);
+        }
     },
-    async create(req, res){
+    async create(req, res) {
         const { fullname, email, password } = req.body;
         await connection('teachers').insert({
             fullname,
@@ -13,8 +20,8 @@ module.exports = {
         });
         return res.json({ fullname, email, password });
     },
-    async update(req,res){
-        const {id} = req.params;
+    async update(req, res) {
+        const { id } = req.params;
         const { fullname, email, password } = req.body;
         const professores = await connection('teachers')
             .where('id', id)
@@ -24,11 +31,11 @@ module.exports = {
             fullname,
             email,
             password
-        }).where('id',id);
-        return res.json({fullname, email, password});
+        }).where('id', id);
+        return res.json({ fullname, email, password });
     },
-    async delete(req, res){
-        const {id} = req.params;
+    async delete(req, res) {
+        const { id } = req.params;
         const professores = await connection('teachers')
             .where('id', id)
             .first();
--- frontend/src/pages/Professores/EditarProfessores/index.js
+++ frontend/src/pages/Professores/EditarProfessores/index.js
@@ -3,40 +3,50 @@ import { useHistory } from 'react-router-dom';
 import './index.css';
 import Header from '../../../components/Header';
 import api from '../../../services/api';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 
-function EditarProfessores() {  
+function EditarProfessores(props) {
     const [fullname, setFullname] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const teacherEdit = props.match.params.id;
     const history = useHistory();
+    useEffect(() => {
+        async function loadTeachers() {
+            const response = await api.get(`/professores/${teacherEdit}`);
+            setFullname(response.data.fullname);
+            setEmail(response.data.email);
+            setPassword(response.data.password);
+        }
+        loadTeachers();
+    }, [])
 
-    async function handleAddTeacher(e){
+
+    async function handleEditTeacher(e) {
         e.preventDefault();
         const data = {
             fullname,
-            email, 
+            email,
             password
         };
-        try{
-            await api.post('/professores', data);
+        try {
+            await api.put(`professores/${teacherEdit}`, data);
             history.push('/professores');
-        }catch(err){
-            alert('Erro ao cadastrar, tente novamente');
+        } catch (err) {
+            alert('Erro ao atualizar, tente novamente');
         }
-            
-    }
 
+    }
     return (
         <>
             <Header />
             <section id="container">
                 <section className="cadastro">
-                    <form onSubmit={handleAddTeacher}>
+                    <form onSubmit={handleEditTeacher}>
                         <input type="text" placeholder="Nome Completo: " value={fullname} onChange={e => setFullname(e.target.value)} />
                         <input type="text" placeholder="E-mail: " value={email} onChange={e => setEmail(e.target.value)} />
                         <input type="password" placeholder="Senha: " value={password} onChange={e => setPassword(e.target.value)} />
-                        <button type="submit" className="button-green">Acessar</button>
-                        <button type="reset" className="button-red">Limpar</button>
+                        <button type="submit" className="button-green">Editar</button>
                     </form>
                 </section>
             </section>
