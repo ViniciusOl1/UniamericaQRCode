68b65babedfe115fc9ebf34e2a6a14a6a07ad1e8
Vinicius Oliveira de Freitas <vinicius.vof@outlook.com>
2020-05-19 22:47:54 -0300

    Remoção bibliotecas desnecessárias backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,12 +3,9 @@ const cors = require('cors');
 const routes = require('./routes.js');
 const app = express();
 const port = 3333;
-const morgan = require("morgan");
 app.use(cors());
 app.use(express.json());
-app.use(routes);
-app.use(require("./routes"));
 app.use(express.urlencoded( { extended: true } ));
-app.use(morgan('dev'));
+app.use(routes);
 
 app.listen(port);
