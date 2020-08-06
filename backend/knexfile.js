a0e2163b76ef3227fc39a37a4184c187cdd9f479
Vinicius Oliveira de Freitas <vinicius.vof@outlook.com>
2020-08-05 21:43:18 -0300

    make it better

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -3,14 +3,13 @@
 module.exports = {
 
   development: {
-    client: 'sqlite3',
+    client: 'pg',
     connection: {
-      filename: './src/database/db.sqlite'
-    },
-    migrations: {
-      directory: './src/migrations',
-    },
-    useNullAsDefault: true
+      host : 'ec2-3-222-150-253.compute-1.amazonaws.com',
+      user : 'fufpcfmtandfoh',
+      password : '09e2c799b08aa8bb6f3a492903ee44ca570fe91ee9e3d682c6476ea61be6006e',
+      database : 'dccdh4r2kas70v'
+    }
   },
 
   staging: {
